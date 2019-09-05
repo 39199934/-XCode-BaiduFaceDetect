821b70cfd62578db8ed63a80519a9023376d5c76
rolodestar <39199934@qq.com>
2019-09-05 20:47:17 +0800

    Initial Commit

---
+++ BaiduFaceDetect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaiduFaceDetect
+//
+//  Created by rolodestar on 2019/9/5.
+//  Copyright © 2019 Rolodestar Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
